33f7c1e62ecf2df5af638f597209ecc44c29c35b
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-07-19 11:03:26 +0700

    ratting bar

---
+++ src/app/rating/rating.component.ts
@@ -0,0 +1,59 @@
+
+import {Component, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {IRating} from "../irating";
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent implements OnInit, OnChanges {
+
+  name:String = 'hello'
+
+  @Input()
+  max=10;
+  @Input()
+  current=0;
+
+
+  ratingList: Array<IRating> = []
+
+  constructor() {
+    this.loadRating();
+
+  }
+
+  ngOnInit(): void {
+// this.loadRating();
+  }
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    // if('max' in changes){}
+    // let max= changes.max.currentValue;
+    // max = typeof max==='undefined'?4:max;
+    // this.max = max;
+    this.loadRating();
+  }
+
+  loadRating(){
+    this.ratingList = Array.from({length: this.max}, (_, index) => ({
+      value:index +1,
+      active: index<this.current
+    }));
+  }
+
+  clickChuot(index: number){
+    this.current = index+1;
+    this.ratingList.forEach((item, idx)=>item.active = idx <this.current);
+
+  }
+
+  diChuyenChuot(index: number){
+    this.ratingList.forEach((item, idx)=>item.active = idx <index);
+  }
+
+
+
+}
